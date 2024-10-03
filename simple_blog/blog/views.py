24fe2955d392efb8ec089cfe1e195fc943304b0d
hiki7 <rsaliyev03@gmail.com>
2024-10-03 18:26:50 +0500

    add: post_delete view

--- simple_blog/blog/views.py
+++ simple_blog/blog/views.py
@@ -58,3 +58,11 @@ def post_edit(request, pk):
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
