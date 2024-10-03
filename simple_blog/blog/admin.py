c0e7d93121ee0264750ad86f6ff75f7f22155e9b
hiki7 <rsaliyev03@gmail.com>
2024-10-03 18:57:28 +0500

    add: registered post

--- simple_blog/blog/admin.py
+++ simple_blog/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'content')
+
