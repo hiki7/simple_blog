147065bb46319d4304d6513b90c1174a83f5dfa5
hiki7 <rsaliyev03@gmail.com>
2024-10-03 19:05:18 +0500

    add: follow model

--- simple_blog/users/models.py
+++ simple_blog/users/models.py
@@ -8,3 +8,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.follower.username} follows {self.following.username}'
