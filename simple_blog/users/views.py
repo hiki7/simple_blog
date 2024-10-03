8e7e23fdf7c9166ac0a06211e4ecae4e6ae8fb70
hiki7 <rsaliyev03@gmail.com>
2024-10-03 19:10:32 +0500

    add: register view

--- simple_blog/users/views.py
+++ simple_blog/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/registration.html', {'form': form})
