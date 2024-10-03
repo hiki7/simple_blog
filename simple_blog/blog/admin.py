2705789b6dd613729ef34062c92e240a13f2406e
hiki7 <rsaliyev03@gmail.com>
2024-10-03 18:57:41 +0500

    add: registered comment

--- simple_blog/blog/admin.py
+++ simple_blog/blog/admin.py
@@ -7,3 +7,8 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('author', 'created_at')
     search_fields = ('title', 'content')
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    list_filter = ('post', 'author', 'created_at')
+    search_fields = ('content',)
