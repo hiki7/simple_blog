60b4260795ee8caf50e1b45c1b888fe23e46f344
hiki7 <rsaliyev03@gmail.com>
2024-10-03 18:17:04 +0500

    add: post_list view

--- simple_blog/blog/views.py
+++ simple_blog/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from models import Post
+
+
+def post_list(request):
+    query = request.GET.get('q', '')
+    if query:
+        posts = Post.objects.filter(title__icontains=query)
+    else:
+        posts = Post.objects.all().order_by('-created_at')
+
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'posts': posts, 'query': query})
