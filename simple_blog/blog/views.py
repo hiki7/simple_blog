db1df12a643a17441645e560b02a4b7794281cea
hiki7 <rsaliyev03@gmail.com>
2024-10-03 18:21:01 +0500

    add: post_detail view

--- simple_blog/blog/views.py
+++ simple_blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from models import Post
 
@@ -15,3 +15,20 @@ def post_list(request):
     page_number = request.GET.get('page')
     posts = paginator.get_page(page_number)
     return render(request, 'blog/post_list.html', {'posts': posts, 'query': query})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all().order_by('-created_at')
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post,
+                                                     'comments': comments,'comment_form': comment_form})
