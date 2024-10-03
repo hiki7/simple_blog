5b169711fcd945bb00d825893958fc459ac2b267
hiki7 <rsaliyev03@gmail.com>
2024-10-03 18:05:17 +0500

    fix: change 'related_name' parameter

--- simple_blog/blog/models.py
+++ simple_blog/blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_names='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
