637c4d78e1b2319a3210b26e796e371133c0e6be
hiki7 <rsaliyev03@gmail.com>
2024-10-03 18:03:52 +0500

    add: comment model

--- simple_blog/blog/models.py
+++ simple_blog/blog/models.py
@@ -15,3 +15,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_names='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name_plural = 'Comments'
+        verbose_name = 'Comment'
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
